20e950a715b9a6c52eda218706f7cc99a508249c
GamaunG <61787349+GamaunG@users.noreply.github.com>
2024-04-15 19:52:35 +0300

    add hyprland config

---
+++ .config/hypr/scripts/killactive.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if [ "$(hyprctl activewindow -j | jq -r ".class")" = "Steam" ]; then
+    xdotool getactivewindow windowunmap
+else
+    hyprctl dispatch killactive ""
+fi
---
+++ .config/hypr/scripts/xdg-portal-fix.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sleep 1
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gnome
+killall -e xdg-desktop-portal-kde
+killall -e xdg-desktop-portal-lxqt
+killall -e xdg-desktop-portal-wlr
+killall -e xdg-desktop-portal-gtk
+killall -e xdg-desktop-portal
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal &
