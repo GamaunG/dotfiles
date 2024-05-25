a45ae321b4256ade07e489b4db6a017bef6e23fc
GamaunG <61787349+GamaunG@users.noreply.github.com>
2024-05-25 04:50:56 +0300

    new nvim mappings and plugins

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -2,27 +2,108 @@
 local M = {}
 
 M.general = {
-  n = {
-    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<F6>"] = { "<cmd> set spell! <CR>", "Toggle spellcheck", opts = { nowait = true } },
-    ["<F5>"] = { ':exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', "Toggle nu and rnu" },
+	n = {
+		-- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+		["<F6>"] = { "<cmd> set spell! <CR>", "Toggle spellcheck", opts = { nowait = true } },
+		["<F5>"] = { ':exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', "Toggle nu and rnu" },
+		["<leader>s"] = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "Replace", opts = { nowait = true } },
 
-    --  format with conform
-    ["<leader>fm"] = {
-      function()
-        require("conform").format()
-      end,
-      "formatting",
-    }
+		["<M-j>"] = { "<cmd>cnext<CR>", "cnext" },
+		["<M-k>"] = { "<cmd>cprev<CR>", "cprev" },
+		["<leader>j"] = { "<cmd>lnext<CR>", "lnext" },
+		["<leader>k"] = { "<cmd>lprev<CR>", "lprev" },
 
-  },
-  i = {
-    ["<F5>"] = { '<C-o>:exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', "Toggle nu and rnu" },
-    ["<F6>"] = { "<cmd> set spell! <CR>", "Toggle spellcheck", opts = { nowait = true } },
-  },
-  v = {
-    [">"] = { ">gv", "indent"},
-  },
+		["<leader>u"] = { "<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>", "UndotreeToggle" },
+
+		["<leader>rn"] = {
+			function()
+				require("nvchad.renamer").open()
+			end,
+			"LSP rename",
+		},
+
+		-- Remaping some defaults and adding new telescope binds
+		["gr"] = {
+			function()
+				require("telescope.builtin").lsp_references()
+			end,
+			"LSP references",
+		},
+
+		["<leader>ds"] = {
+			function()
+				require("telescope.builtin").lsp_document_symbols()
+			end,
+			"Document Symbols",
+		},
+
+		["<leader>ws"] = {
+			function()
+				require("telescope.builtin").lsp_dynamic_workspace_symbols()
+			end,
+			"Workspace Symbols",
+		},
+
+		["<leader>q"] = {
+			function()
+				require("telescope.builtin").diagnostics()
+			end,
+			"Diagnostic",
+		},
+
+		["<leader>Q"] = {
+			function()
+				require("telescope.builtin").quickfix()
+			end,
+			"Quickfix list",
+		},
+
+		-- Harpoon bindings
+		["<leader>h"] = {"",""},
+
+		["<leader>hm"] = {
+			function()
+				require("harpoon.ui").toggle_quick_menu()
+			end,
+			"Harpoon Menu",
+		},
+
+		["<leader>ha"] = {
+			function()
+				require("harpoon.mark").add_file()
+			end,
+			"Harpoon Add file",
+		},
+
+		["<M-p>"] = {
+			function()
+				require("harpoon.ui").nav_prev()
+			end,
+			"Harpoon Prev file",
+		},
+
+		["<M-n>"] = {
+			function()
+				require("harpoon.ui").nav_next()
+			end,
+			"Harpoon Next file",
+		},
+
+		--  format with conform
+		["<leader>fm"] = {
+			function()
+				require("conform").format()
+			end,
+			"formatting",
+		},
+	},
+	i = {
+		["<F5>"] = { '<C-o>:exec &nu==&rnu? "se nu!" : "se rnu!"<CR>', "Toggle nu and rnu" },
+		["<F6>"] = { "<cmd> set spell! <CR>", "Toggle spellcheck", opts = { nowait = true } },
+	},
+	v = {
+		[">"] = { ">gv", "indent" },
+	},
 }
 
 -- more keybinds!
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -3,74 +3,86 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-    enabled = false,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+		enabled = false,
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-    enabled = false,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+		enabled = false,
+	},
 
-  {
-    "stevearc/conform.nvim",
-    --  for users those who want auto-save conform + lazyloading!
-    -- event = "BufWritePre"
-    cmd = { "ConformInfo" },
-    config = function()
-      require "custom.configs.conform"
-    end,
-    -- enabled = false,
-  },
+	{
+		"stevearc/conform.nvim",
+		--  for users those who want auto-save conform + lazyloading!
+		-- event = "BufWritePre"
+		cmd = { "ConformInfo" },
+		config = function()
+			require("custom.configs.conform")
+		end,
+		-- enabled = false,
+	},
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
 
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-  },
+	{
+		"mbbill/undotree",
+		cmd = { "UndotreeToggle", "UndotreeShow", "UndotreeFocus" },
+	},
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	{
+		"theprimeagen/harpoon",
+		lazy = false
+	},
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+
+	-- All NvChad plugins are lazy-loaded by default
+	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+	-- {
+	--   "mg979/vim-visual-multi",
+	--   lazy = false,
+	-- }
 }
 
 return plugins
